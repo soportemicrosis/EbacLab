5853414ec5912fb1fd605c8ba33a66e1c3c3ef0c
Soporte Microsistemas <145878765+soportemicrosis@users.noreply.github.com>
2025-04-21 08:41:33 -0600

    actualizando el calculo de suma resta multiplicacionn y division 1.0

---
+++ calculo.py
@@ -0,0 +1,11 @@
+#calcular suma resta y multiplicación de dos números
+a=int(input("ingrese a "))
+b=int(input("ingrese b "))
+2
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+
+print("la suma es ",suma)
+print("la suma es ",resta)
+print("la suma es ",multiplicacion)
