31bfd8f956096561b10af1abc7719a9dcef49a75
RobertioCarlos <robertio.carlos@gmail.com>
2025-04-23 17:28:22 -0600

    Cambio realizado

--- tablamultiplica.py
+++ tablamultiplica.py
@@ -2,6 +2,7 @@
 
 x=int(input("Ingrese un numero: "))
 
-for i in range(0,11):
+for i in range(1,11):
     print(i," x ",x," = ",x*i)
     #El rango comienzaa en cero por favor cambiarlo a 1 QA
+    #Cambio realizado
