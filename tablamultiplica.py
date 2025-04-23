7098ce8ad4c81b5bfbd126d660cc34b66aa031f0
RobertioCarlos <robertio.carlos@gmail.com>
2025-04-23 17:40:20 -0600

    ultimo cambio

--- tablamultiplica.py
+++ tablamultiplica.py
@@ -5,4 +5,4 @@ x=int(input("Ingrese un numero: "))
 for i in range(1,11):
     print(i," x ",x," = ",x*i)
     #El rango comienzaa en cero por favor cambiarlo a 1 QA
-    #Cambio realizado
+    #Cambio realizado se cambio el cero por uno
