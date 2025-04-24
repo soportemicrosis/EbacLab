14574cc53fa78839bf5d0afa4c979cd7dbc21a28
RobertioCarlos <robertio.carlos@gmail.com>
2025-04-23 18:07:22 -0600

    cambios previos al pull rebase

--- tablamultiplica.py
+++ tablamultiplica.py
@@ -5,4 +5,4 @@ x=int(input("Ingrese un numero: "))
 for i in range(0,11):
     print(i," x ",x," = ",x*i)
 print("Adios!")
-    #El rango comienzaa en cero por favor cambiarlo a 1
+    #El rango comienzaa en cero por favor cambiarlo a 1 
